690bc9c4aa833dc13e83f6576299015c664413c8
unknown <you@example.com>
2026-02-27 21:53:17 +0600

    set up for hero section

---
+++ app/(frontned)/(site)/faq/page.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function AboutPage() {
+  return (
+    <div className=" min-h-screen">
+
+      {/* Hero Section */}
+
+        <h1 className="text-4xl md:text-5xl font-bold text-gray-800">
+          This is faq
+        </h1>
+        
+
+    </div>
+  );
+}
---
+++ app/components/home/Hero.jsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+
+        <div className='w-full '>
+            <h1>this is hero</h1>
+        </div>
+
+    );
+};
+
+export default Navbar;
